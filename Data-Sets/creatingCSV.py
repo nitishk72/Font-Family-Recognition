8df3bfaeb2fbe5e6b04300b1ba731ed4f18101f6
Nitish Kumar Singh <nitishk72@gmail.com>
2018-05-02 20:30:01 +0530

    csv file added

--- Data-Sets/creatingCSV.py
+++ Data-Sets/creatingCSV.py
@@ -1,18 +1,24 @@
 from scipy import misc
-import csv
+import numpy as np
 
 fonts = ['AcmeFont', 'AgencyFB', 'Algerian', 'ArialBlack', 'Broadway', 'Calibri', 'Itaianate']
 alpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
          'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
          'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
          'Y', 'Z']
+data = []
+i = 0
+for char in alpha:
+    # data.append([])
+    img = misc.imread(fonts[0]+'\\'+char+'.jpg')
+    img = misc.imresize(img, (8, 8))
+    img = img.astype(float)
+    img = misc.bytescale(img, low=0, high=16)
+    x = []
+    for each_row in img:
+        for each_column in each_row:
+            x.append(sum(each_column)/3.0)
+    data.append(x)
 
-img = misc.imread('Data-Sets\\'+fonts[0]+'\\'+alpha[0]+'.jpg')
-img = misc.imresize(img, (8, 8))
-img = img.astype(float)
-img = misc.bytescale(img, low=0, high=16)
-
-x = []
-for each_row in img:
-    for each_column in each_row:
-        x.append(sum(each_column)/3.0)
+data2D = np.asarray(data)
+np.savetxt("foo.csv", data2D, delimiter=",")
